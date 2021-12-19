43cbe68c0f45a5c787f8f7cd9d1b48c770a55f15
madams <mtadams42@gmail.com>
2021-12-19 12:22:54 -0700

    Added the corner. Now it's time to start real dev...

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 	let name: string = "world";
+	import GithubCorner from "./GithubCorner.svelte";
 </script>
 
 <main>
+	<GithubCorner href="https://github.com/loremdipso/secret_santa" />
 	<h1>Hello {name}!</h1>
 	<p>
 		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
